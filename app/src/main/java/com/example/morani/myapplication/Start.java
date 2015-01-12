3f1cd643a1e2446971b481205e2e53e27cd36290
moranshor <moranshor@gmail.com>
2015-01-12 12:23:52 +0200

    fdf

---
+++ app/src/main/java/com/example/morani/myapplication/Start.java
@@ -0,0 +1,15 @@
+package com.example.morani.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by morani on 06/01/2015.
+ */
+public class Start extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
